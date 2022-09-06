5a7070318a341674cd6152485b7c9e0134d3ab65
ry0y4n <rusk.ryoy4n@gmail.com>
2022-09-06 11:18:05 +0900

    typo

--- src/taskparameters.ts
+++ src/taskparameters.ts
@@ -50,9 +50,9 @@ export class TaskParameters {
         console.log('Optional ingress parameters')
         // Optional ingress parameters
         console.log(core.getInput('ingress-external', { required: false }))
-        console.log(core.getInput('ingress-external', { required: false })==true)
-        console.log(core.getBooleanInput('booleanInputName', { required: false }))
-        console.log(core.getBooleanInput('booleanInputName', { required: false })==true)
+        console.log(core.getInput('ingress-external', { required: false })=="true")
+        console.log(core.getBooleanInput('ingress-external', { required: false }))
+        console.log(core.getBooleanInput('ingress-external', { required: false })=="true")
         this._ingressExternal = core.getInput('ingress-external', { required: false }) == "true";
         this._ingressTargetPort = parseInt(core.getInput('ingress-target-port', { required: false }));
         let ingressCustomDomainsJsonString = core.getInput('ingress-custom-domains-json', { required: false });
