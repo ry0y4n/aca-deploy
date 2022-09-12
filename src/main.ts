6af548176bca7602c38280b93d324799419151fb
ry0y4n <rusk.ryoy4n@gmail.com>
2022-09-12 14:34:56 +0900

    fix(console custom domain setting)

--- src/main.ts
+++ src/main.ts
@@ -108,7 +108,7 @@ async function main() {
     );
     if (containerAppDeploymentResult.provisioningState == "Succeeded") {
       console.log("Deployment Succeeded");
-      console.log(containerAppDeploymentResult.configuration.ingress.customDomains.name)
+      console.log(containerAppDeploymentResult.configuration?.ingress?.customDomains[0]?.name)
 
       if (ingresConfig.external == true) {
         let appUrl = "http://"+containerAppDeploymentResult.latestRevisionFqdn+"/"
