eef0e918a94d539fed3d33bc1b7541e0d9296815
ry0y4n <rusk.ryoy4n@gmail.com>
2022-09-12 14:53:56 +0900

    console scale rule setting

--- src/main.ts
+++ src/main.ts
@@ -108,7 +108,7 @@ async function main() {
     );
     if (containerAppDeploymentResult.provisioningState == "Succeeded") {
       console.log("Deployment Succeeded");
-      console.log(containerAppDeploymentResult.configuration?.ingress?.customDomains)
+      console.log(containerAppDeploymentResult.template?.scale?.rules)
 
       if (ingresConfig.external == true) {
         let appUrl = "http://"+containerAppDeploymentResult.latestRevisionFqdn+"/"
